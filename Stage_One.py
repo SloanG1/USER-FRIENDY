ff442880345f3b6cc5b4ca09aa9b6008adbed059
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2024-04-13 09:22:00 -0500

    ;lkasdf

--- Stage_One.py
+++ Stage_One.py
@@ -10,6 +10,7 @@ Py_Window = pygame.display.set_mode((WIDTH, HEIGHT))  # Create py window
 pygame.display.set_caption("USER FRIENDY")  # Window Game Caption
 name_dict = {'test':23}
 
+
 class Stage_One:
     # Data Attributes
     __Name_Dict = {}
@@ -63,7 +64,6 @@ class Stage_One:
         textRect = text.get_rect()
         textRect.center = (WIDTH // 2, HEIGHT // 1.5)
         Py_Window.blit(text, textRect)
-
         name_box = pygame.Rect(200, 200, 140, 32)
         user_text = ''
         while True:
@@ -78,6 +78,7 @@ class Stage_One:
                     # Check for backspace
                     if event.key == pygame.K_RETURN:
                         if user_text in name_dict:
+                            Stage_One.update_DoB()
                             return False
                         else:
                             return False
@@ -97,10 +98,43 @@ class Stage_One:
             name_box.w = max(100, text_surface.get_width() + 10)
             pygame.display.update()
 
-            @staticmethod
-            def update_DoB():
+    @staticmethod
+    def update_DoB():
+        Py_Window.fill((0, 0, 0))  # Set Background color
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        text = font.render('Enter DoB', True, (255, 255, 255), (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (WIDTH // 2, HEIGHT // 3)
+        Py_Window.blit(text, textRect)
 
+        name_box = pygame.Rect(200, 200, 140, 32)
+        user_text = ''
+        while True:
+            for event in pygame.event.get():
 
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+
+                    # Check for backspace
+                    if event.key == pygame.K_RETURN:
+                        return False
+                    else:
+                        user_text += event.unicode
+
+            pygame.draw.rect(Py_Window, [0, 0, 0], name_box)
+
+            text_surface = pygame.font.Font('freesansbold.ttf', 32).render(user_text, True, (255, 255, 255))
+
+            # render at position stated in arguments
+            Py_Window.blit(text_surface, (WIDTH // 2, HEIGHT // 2))
+
+            # set width of textfield so that text cannot get
+            # outside of user's text input
+            name_box.w = max(100, text_surface.get_width() + 10)
+            pygame.display.update()
 
     # Getters
     def get_name_dict(self):
